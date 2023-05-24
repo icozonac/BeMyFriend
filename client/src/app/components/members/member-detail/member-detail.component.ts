3d58ebacad81b9b9b53ff0fa2e8cbdaa9c86c651
Cozonac Ion <fresh.go.ionutz@gmail.com>
2023-05-24 22:53:28 +0300

    add code to client

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,5 +1,8 @@
+import { MembersService } from 'src/app/services/members.service';
 import { Member } from './../../../models/member';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -8,8 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: { small: string; medium: string; big: string }[] = [];
 
-  constructor() {}
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    if (!this.member) return [];
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+      });
+    }
+    return imageUrls;
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: (member) => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      },
+    });
+  }
 }
