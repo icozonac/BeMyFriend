7dce7ba79a667d7af57612f2b3b16e1040cc27dc
Cozonac Ion <fresh.go.ionutz@gmail.com>
2023-05-29 14:23:06 +0300

    add paging, sorting and filtering

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member';
+import { Pagination } from 'src/app/models/pagination';
+import { User } from 'src/app/models/user';
+import { UserParams } from 'src/app/models/userParams';
 import { MembersService } from 'src/app/services/members.service';
 
 @Component({
@@ -9,13 +11,49 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-list.component.scss'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]> | undefined;
+  // members$: Observable<Member[]> | undefined;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  userParams: UserParams | undefined;
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
-  constructor(private membersService: MembersService) {}
-
-  ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+  constructor(private membersService: MembersService) {
+    this.userParams = this.membersService.getUserParams();
   }
 
+  ngOnInit(): void {
+    // this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
 
+  loadMembers() {
+    if (this.userParams) {
+      this.membersService.setUserParams(this.userParams);
+      this.membersService.getMembers(this.userParams).subscribe({
+        next: (response) => {
+          if (response.result && response.pagination) {
+            this.members = response.result;
+            this.pagination = response.pagination;
+          }
+        },
+      });
+    }
+  }
+
+  resetFilters() {
+    this.userParams = this.membersService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
+    if (this.userParams && this.userParams?.pageNumber !== event.page) {
+      window.scrollTo(0, 0);
+      this.userParams.pageNumber = event.page;
+      this.membersService.setUserParams(this.userParams);
+      this.loadMembers();
+    }
+  }
 }
