83356100ca69f140e718ddb7976d81281e769d09
Cozonac Ion <fresh.go.ionutz@gmail.com>
2023-06-08 16:12:28 +0300

    publishing

--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.WithOrigins("https://localhost:4200").AllowAnyHeader()
@@ -27,9 +28,13 @@ app.UseCors(builder => builder.WithOrigins("https://localhost:4200").AllowAnyHea
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -39,7 +44,8 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+
+    await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
