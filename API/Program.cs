35c3c3bb610252794bd73cbf6b7a5199bd2f7a78
Cozonac Ion <fresh.go.ionutz@gmail.com>
2023-06-09 14:05:48 +0300

    add appsettings.json

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+}
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.WithOrigins("https://localhost:4200").AllowAnyHeader()
